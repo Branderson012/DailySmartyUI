6058d43a3af7e933e34c72dbd7a19d34b4651c0c
Brandon Anderson <you@example.commusic_fourlife@yahoo.com>
2019-12-12 20:02:06 -0800

    Creating Searchbar and Recent Posts Components

---
+++ src/components/searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="Search Dailysmarty"></input>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
